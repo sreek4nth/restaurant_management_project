eb0cf5093c3dde4611bb74c1e52bdb5cbef3dffc
sreek4nth <144669087+sreek4nth@users.noreply.github.com>
2025-08-11 16:09:10 +0530

    28724

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -1,12 +1,12 @@
 import os
 import django
 from django.conf import settings
-from django.http import Httpresponse
+from django.http import HttpResponse
 from django.urls import path
 from django.core .management import execute_from_command_line
 
 BASE_DIR = os.path.dirname(__file__)
-settings.configure
+settings.configure(
     DEBUG=True,
     SECRET_KEY=__name__,
     ALLOWED_HOSTS=['*'],
@@ -25,11 +25,11 @@ settings.configure
 django.setup()
 
 def home(request):
-    return Httpresponse('<h1>welocme to my homepage </h1>')
+    return Httpresponse('<h1>welcome to my homepage </h1>')
 
 urlpatterns=[
     path('',home),
 ]    
 
-if__name__="__main__":
-    execute_from_command_line([__file__m,'runserver','0.0.0.0:8000'])
+if__name__ = "__main__":
+    execute_from_command_line([__file__,'runserver','0.0.0.0:8000'])

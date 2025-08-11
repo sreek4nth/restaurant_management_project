aac3e47156a79fe0c5a220418217ad16168a22d2
sreek4nth <144669087+sreek4nth@users.noreply.github.com>
2025-08-11 16:04:37 +0530

    28725

---
+++ restaurant_management/main.py
@@ -0,0 +1,35 @@
+import os
+import django
+from django.conf import settings
+from django.http import Httpresponse
+from django.urls import path
+from django.core .management import execute_from_command_line
+
+BASE_DIR = os.path.dirname(__file__)
+settings.configure
+    DEBUG=True,
+    SECRET_KEY=__name__,
+    ALLOWED_HOSTS=['*'],
+    MIDDLEWARE=[
+        'django.middleware.commom.CommonMiddleware',
+    ],
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS':[os.path.join(BASE_DIR,'templates')],
+            'APP_DIRS':True,
+        },
+    ],
+)
+
+django.setup()
+
+def home(request):
+    return Httpresponse('<h1>welocme to my homepage </h1>')
+
+urlpatterns=[
+    path('',home),
+]    
+
+if__name__="__main__":
+    execute_from_command_line([__file__m,'runserver','0.0.0.0:8000'])

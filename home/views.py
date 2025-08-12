5251dde7f00d6bf17c1cd37455b8d0aa2e306c82
sreek4nth <144669087+sreek4nth@users.noreply.github.com>
2025-08-12 22:45:47 +0530

    28727

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from .models import Restaurant
 
 # Create your views here.
 
 def home(request):
-    return(request,"home.html")
+    return render(request,"home.html")
     
 def home_view(request):
-    return HttpResponse(f"<h1> welcome to {settings.RESTAURANT_NAME}</h1>:")
+    restaurant = Restaurant.objects.first()
+    if restaurant:
+        return HttpResponse(f"<h1>welcome to{restaurant.name}</h1>")
+    else:
+        return HttpResponse("<h1>welcome to our Restaurant</h1>")
+

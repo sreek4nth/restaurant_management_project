b84240bdbe4e867ef2aa753d1b328a57220ff77a
sreek4nth <144669087+sreek4nth@users.noreply.github.com>
2025-08-12 16:13:00 +0530

    28727

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+def home_view(request):
+    return HttpResponse(f"<h1>welcome to{settings.RESTAURANT_NAME}</h1>")
